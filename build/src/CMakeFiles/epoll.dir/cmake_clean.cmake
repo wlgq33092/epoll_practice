bc61a354e57b661abf0c47d090dd1a8cac7ac72d
wlgq33092 <wgfly1989@gmail.com>
2015-10-26 23:59:19 +0800

    create project

---
+++ build/src/CMakeFiles/epoll.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/epoll.dir/main.o"
+  "epoll.pdb"
+  "epoll"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/epoll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
